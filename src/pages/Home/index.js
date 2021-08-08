61e03f60ef25f62bc67c981d97a5e66759a306ab
Lucas Felipe Costa <lucasfelipecost4@hotmail.com>
2021-08-07 23:53:17 -0300

    adicionando a logo e a busca

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,27 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useState } from 'react';
+import TextField, { Input } from '@material/react-text-field';
 
-const Home = () => <Container>Hello</Container>;
+import { Container, Search } from './styles';
+import logo from '../../assets/logo.svg';
+
+const Home = () => {
+  const [inputValue, setInputValue] = useState();
+
+  return (
+    <Container>
+      <Search>
+        <img src={logo} alt="Logo do Site" />
+        <TextField
+          label="Pesquisar"
+          outlined
+          // onTrailingIconSelect={() => this.setState({ value: '' })}
+          // trailingIcon={<MaterialIcon role="button" icon="delete" />}
+        >
+          <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+        </TextField>
+      </Search>
+    </Container>
+  );
+};
 
 export default Home;
