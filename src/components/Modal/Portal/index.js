d4452f11b70297861e2e610a710cfb5ee2450fff
Lucas Felipe Costa <lucasfelipecost4@hotmail.com>
2021-08-14 00:36:18 -0300

    limpando cash

--- src/components/Modal/Portal/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom';
-
-const PortalModel = ({ children }) => {
-  const portal = document.getElementById('modal-root');
-  return ReactDOM.createPortal(children, portal);
-};
-
-export default PortalModel;
--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as ImageCard } from './ImageCard';
-export { default as RestauranteCard } from './RestauranteCard';
-export { default as Modal } from './Modal';
