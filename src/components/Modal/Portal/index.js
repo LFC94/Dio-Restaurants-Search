9dafa87bf1b6afdc4714b313feb5fe3ae55bbb04
Lucas Felipe Costa <lucasfelipecost4@hotmail.com>
2021-08-13 23:03:14 -0300

    Criando o scopo das modal

---
+++ src/components/Modal/Portal/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+
+const PortalModel = ({ children }) => {
+  const portal = document.getElementById('modal-root');
+  return ReactDOM.createPortal(children, portal);
+};
+
+export default PortalModel;
---
+++ src/components/Modal/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Portal from './Portal';
+
+const Modal = () => {
+  return (
+    <Portal>
+      <div>fff</div>
+    </Portal>
+  );
+};
+
+export default Modal;
